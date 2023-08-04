1d11d33539f4add0ffe57b43730b4549df48e96c
Francesc Rocher <francesc.rocher@gmail.com>
2023-08-04 18:33:25 +0200

    ✅  [tests] Add Lorem Ipsum tests

--- tests/src/variables_tests.adb
+++ tests/src/variables_tests.adb
@@ -38,6 +38,8 @@ package body Variables_Tests is
         (T, Test_No_Substitution'Access, "No Substitutions Expected");
       Register_Routine
         (T, Test_Basic_Substitution'Access, "Basic Substitutions");
+      Register_Routine
+        (T, Test_Modal_Substitution'Access, "Modal Substitutions");
    end Register_Tests;
 
    --------------------------
@@ -50,7 +52,7 @@ package body Variables_Tests is
       Expected : aliased Mold.Results_Type;
    begin
 
-      --  source => no-vars.txt.mold, definitions => empty.toml
+      --  ----- no variables in the source file -------------------------------
       --!pp off
       Errors := Mold.Apply (
          Source      => "suite/mold/no-vars.txt.mold",
@@ -78,7 +80,7 @@ package body Variables_Tests is
       Check_MD5_Digest
         ("suite/tmp/no-vars.txt", "7ef8e151c0fde9d5fef738709a321300");
 
-      --  source => foo.txt.mold, definitions => empty.toml
+      --  ----- empty definitions file ----------------------------------------
       --!pp off
       Errors := Mold.Apply (
          Source      => "suite/mold/foo.txt.mold",
@@ -106,7 +108,7 @@ package body Variables_Tests is
       Check_MD5_Digest
         ("suite/tmp/foo.txt", "4c179dd0c4cc0c668539a25435286258");
 
-      --  source => foo.txt.mold, definitions => bar.toml
+      --  ----- no variable can be replaced -----------------------------------
       --!pp off
       Errors := Mold.Apply (
          Source      => "suite/mold/foo.txt.mold",
@@ -146,7 +148,7 @@ package body Variables_Tests is
       Expected : aliased Mold.Results_Type;
    begin
 
-      --  source => foo.txt.mold, definitions => foo.toml
+      --  ----- variable replaced ---------------------------------------------
       --!pp off
       Errors := Mold.Apply (
          Source      => "suite/mold/foo.txt.mold",
@@ -174,7 +176,7 @@ package body Variables_Tests is
       Check_MD5_Digest
         ("suite/tmp/foo.txt", "3d22c1e66750c3e7925e643cfbe9e327");
 
-      --  source => foo-bar.txt.mold, definitions => foo.toml
+      --  ----- four variables, two are replaced ------------------------------
       --!pp off
       Errors := Mold.Apply (
          Source      => "suite/mold/foo-bar.txt.mold",
@@ -202,7 +204,7 @@ package body Variables_Tests is
       Check_MD5_Digest
         ("suite/tmp/foo-bar.txt", "9fe90f7706a6c0de1155e8e340fafed7");
 
-      --  source => foo-bar.txt.mold, definitions => foo-bar.toml
+      --  ----- all variables replaced ----------------------------------------
       --!pp off
       Errors := Mold.Apply (
          Source      => "suite/mold/foo-bar.txt.mold",
@@ -231,5 +233,102 @@ package body Variables_Tests is
         ("suite/tmp/foo-bar.txt", "5b6c9393c2233d09b1517bc8c3ca9de1");
 
    end Test_Basic_Substitution;
+
+   -----------------------------
+   -- Test_Modal_Substitution --
+   -----------------------------
+
+   procedure Test_Modal_Substitution (T : in out Test_Case'Class) is
+      Errors   : Natural;
+      Results  : aliased Mold.Results_Type;
+      Expected : aliased Mold.Results_Type;
+      Settings : aliased Mold.Settings_Type := Global_Settings.all;
+   begin
+
+      --  ----- all variables replaced ----------------------------------------
+      --!pp off
+      Errors := Mold.Apply (
+         Source      => "suite/mold/lorem-ipsum.txt.mold",
+         Output_Dir  => "suite/tmp/",
+         Settings    => Settings'Unchecked_Access,
+         Definitions => "suite/toml/lorem-ipsum.toml",
+         Results     => Results'Unchecked_Access
+      );
+      Expected := [
+         Files       =>    1,
+         Renamed     =>    0,
+         Overwritten =>    0,
+         Definitions =>   26,
+         Variables   => 2118,
+         Undefined   =>    0,
+         Replaced    => 2118,
+         Ignored     =>    0,
+         Emptied     =>    0,
+         Warnings    =>    0,
+         Mold.Errors =>    0
+      ];
+      --!pp on
+      Check_Results
+        (Errors, Results'Unchecked_Access, Expected'Unchecked_Access);
+      Check_MD5_Digest
+        ("suite/tmp/lorem-ipsum.txt", "ff416bfec859c59a3834c46d60250e25");
+
+      --  ----- no optional variables defined ---------------------------------
+      --!pp off
+      Errors := Mold.Apply (
+         Source      => "suite/mold/lorem-ipsum.txt.mold",
+         Output_Dir  => "suite/tmp/",
+         Settings    => Settings'Unchecked_Access,
+         Definitions => "suite/toml/lorem-ipsum_no-opts1.toml",
+         Results     => Results'Unchecked_Access
+      );
+      Expected := [
+         Files       =>    1,
+         Renamed     =>    0,
+         Overwritten =>    1,
+         Definitions =>   23,
+         Variables   => 2118,
+         Undefined   =>  291,
+         Replaced    => 1827,
+         Ignored     =>    0,
+         Emptied     =>  291,
+         Warnings    =>    0,
+         Mold.Errors =>    0
+      ];
+      --!pp on
+      Check_Results
+        (Errors, Results'Unchecked_Access, Expected'Unchecked_Access);
+      Check_MD5_Digest
+        ("suite/tmp/lorem-ipsum.txt", "fee4ce163f4e85103e42ab27a49ee381");
+
+      --  ----- some optional variables defined -------------------------------
+      --!pp off
+      Errors := Mold.Apply (
+         Source      => "suite/mold/lorem-ipsum.txt.mold",
+         Output_Dir  => "suite/tmp/",
+         Settings    => Settings'Unchecked_Access,
+         Definitions => "suite/toml/lorem-ipsum_no-opts2.toml",
+         Results     => Results'Unchecked_Access
+      );
+      Expected := [
+         Files       =>    1,
+         Renamed     =>    0,
+         Overwritten =>    1,
+         Definitions =>   24,
+         Variables   => 2118,
+         Undefined   =>  176,
+         Replaced    => 1942,
+         Ignored     =>    0,
+         Emptied     =>  176,
+         Warnings    =>    0,
+         Mold.Errors =>    0
+      ];
+      --!pp on
+      Check_Results
+        (Errors, Results'Unchecked_Access, Expected'Unchecked_Access);
+      Check_MD5_Digest
+        ("suite/tmp/lorem-ipsum.txt", "1ed55361c952f1e572a156c07a3c2f3d");
+
+   end Test_Modal_Substitution;
 
 end Variables_Tests;
