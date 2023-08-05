afc251e33579c7af34a34f6aa7e2b47ef46a8730
Francesc Rocher <francesc.rocher@gmail.com>
2023-08-05 12:58:36 +0200

    Set inclusion prefix to 'include:'

--- src/mold.ads
+++ src/mold.ads
@@ -15,7 +15,7 @@ package Mold is
    Optional_Substitution_Prefix  : constant Character := '?';
    Mandatory_Substitution_Prefix : constant Character := '#';
    File_Extension                : constant String    := "mold";
-   Inclusion_File_Prefix         : constant String    := "file:";
+   Inclusion_File_Prefix         : constant String    := "include:";
    Include_File_Extension        : constant String    := "molt";
 
    type Settings_Type is record
