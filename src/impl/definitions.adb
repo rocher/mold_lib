aaf39e5df51c2f225b96970e4459cfb1308e4f7b
Francesc Rocher <francesc.rocher@gmail.com>
2023-08-09 09:22:20 +0200

    .. and even more simple

--- src/impl/definitions.adb
+++ src/impl/definitions.adb
@@ -1,86 +1,17 @@
--------------------------------------------------------------------------------
---
---  Mold - Meta-variable Operations for Lean Development (lib)
---  Copyright (c) 2023 Francesc Rocher <francesc.rocher@gmail.com>
---  SPDX-License-Identifier: MIT
---
--------------------------------------------------------------------------------
-
-with Mold;
-
 package body Definitions is
 
-   ----------------------
-   -- Set_Mold_Setting --
-   ----------------------
+   procedure Read_Variables (S : String) is null;
 
-   function Set_Mold_Setting
-     (Key, Value : String; Settings : not null Mold.Settings_Access)
-      return Boolean
-   is
-      Success : Boolean := True;
-
-      -----------------
-      -- Set_Boolean --
-      -----------------
-
-      procedure Set_Boolean
-        (Variable : not null access Boolean; Value : String)
-      is
-      begin
-         null;
-      end Set_Boolean;
-
+   function GNAT_Bug (Key : String) return Boolean is
    begin
+      case Key is
+         when "mold-rename-source" =>
+            null;
 
-      if Settings.Defined_Settings then
-         case Key is
-            when "mold-rename-source" =>
-               Set_Boolean (Settings.Rename_Source'Access, Value);
-            when "mold-delete-source" =>
-               Set_Boolean (Settings.Delete_Source'Access, Value);
-            when "mold-overwrite" =>
-               Set_Boolean (Settings.Overwrite'Access, Value);
-            when "mold-abort-on-error" =>
-               Set_Boolean (Settings.Abort_On_Error'Access, Value);
-
-            when "mold-action" =>
-               case Value is
-                  when "IGNORE" | "Ignore" | "ignore" =>
-                     Settings.Action := Mold.Ignore;
-                  when "EMPTY" | "Empty" | "empty" =>
-                     Settings.Action := Mold.Empty;
-                  when others =>
-                     Log.Error
-                       ("Invalid setting value in " & Key & " = " & Value);
-                     Success := False;
-               end case;
-
-            when "mold-alert" =>
-               case Value is
-                  when "NONE" | "None" | "none" =>
-                     Settings.Alert := Mold.None;
-                  when "WARNING" | "Warning" | "warning" =>
-                     Settings.Alert := Mold.Warning;
-                  when others =>
-                     Success := False;
-               end case;
-
-            when others =>
-               Success := False;
-         end case;
-      end if;
-
-      return Success;
-   end Set_Mold_Setting;
-
-   ------------------------
-   -- Read_Variables_Map --
-   ------------------------
-
-   function Read_Variables return Variables_Map is
-   begin
-      return Empty_Map;
-   end Read_Variables;
+         when others =>
+            null;
+      end case;
+      return True;
+   end GNAT_Bug;
 
 end Definitions;
--- src/impl/definitions.ads
+++ src/impl/definitions.ads
@@ -1,24 +1,5 @@
--------------------------------------------------------------------------------
---
---  Mold - Meta-variable Operations for Lean Development (lib)
---  Copyright (c) 2023 Francesc Rocher <francesc.rocher@gmail.com>
---  SPDX-License-Identifier: MIT
---
--------------------------------------------------------------------------------
-
-with Ada.Containers.Hashed_Maps; use Ada.Containers;
-with Ada.Strings.Unbounded;      use Ada.Strings.Unbounded;
-with Ada.Strings.Unbounded.Hash;
-
 package Definitions is
 
-   package Variables_Package is new Hashed_Maps
-     (Key_Type => Unbounded_String, Element_Type => Unbounded_String,
-      Hash => Ada.Strings.Unbounded.Hash, Equivalent_Keys => "=", "=" => "=");
-
-   subtype Variables_Map is Variables_Package.Map;
-   type Variables_Access is access all Variables_Map;
-
-   function Read_Variables return Variables_Map;
+   procedure Read_Variables (S : String);
 
 end Definitions;
