d0462c4b02243eaf84c17c4ce11980904e5d71ae
Francesc Rocher <francesc.rocher@gmail.com>
2023-10-21 21:49:43 +0200

    Add test for invalid include extension

--- tests/src/errors_tests.adb
+++ tests/src/errors_tests.adb
@@ -149,6 +149,27 @@ package body Errors_Tests is
       Check_Results
         (Errors, Results'Unchecked_Access, Expected'Unchecked_Access, 1);
 
+      --  ----- invalid included file extension -------------------------------
+      Settings.Abort_On_Error := True;
+      Results                 := [others => 0];
+      --!pp off
+      Errors := Apply (
+         Source     => "suite/mold/invalid-include-ext.mold",
+         Output_Dir => "suite/tmp",
+         Settings   => Settings'Unchecked_Access,
+         Toml_File  => "suite/toml/foo.toml",
+         Results    => Results'Unchecked_Access,
+         Log_Level  => Log.Level
+      );
+      Expected := [
+         Files_Processed     => 1,
+         Variables_Defined   => 1,
+         others              => 0
+      ];
+      --!pp on
+      Check_Results
+        (Errors, Results'Unchecked_Access, Expected'Unchecked_Access, 1);
+
    end File_Errors;
 
    ----------------------
