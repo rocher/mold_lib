9c218299526137f8fe79d738f4b5459a24af4b66
Francesc Rocher <francesc.rocher@gmail.com>
2023-10-21 20:56:00 +0200

    Move coverage annotation

--- src/impl/mold_lib-impl-validation.adb
+++ src/impl/mold_lib-impl-validation.adb
@@ -43,8 +43,8 @@ package body Mold_Lib.Impl.Validation is
          end if;
       end return;
 
-   exception
       pragma Annotate (Xcov, Exempt_On, "Only valid in Windows OS");
+   exception
       when E : Dir.Name_Error | Dir.Use_Error =>
          Log_Exception (E, "Invalid directory");
          Error := True;
