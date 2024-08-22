d4905f1c083743918fc97f910cbace2c286d608e
Francesc Rocher <francesc.rocher@gmail.com>
2024-08-22 10:51:39 +0200

    Fix indentation

--- src/impl/mold_lib-impl-file.ads
+++ src/impl/mold_lib-impl-file.ads
@@ -8,17 +8,17 @@
 
 package Mold_Lib.Impl.File is
 
-  function Replace
-   (Source, Output_Dir : not null String_Access) return Boolean;
-  --  In the given Source file name, replace all occurrences of mold
-  --  variables with the value defined in the Variables map. Behaves
-  --  according to the Settings given and updates the Results object.
-  --
-  --  Parameter source is the name of file with extension "mold", e.g.
-  --  "README.md.mold". The name of the generated file is the same without
-  --  the "mold" extension, "README.md", even when there are no variables to
-  --  replace.
-  --
-  --  Return True if the process end successfully (no error detected).
+   function Replace
+     (Source, Output_Dir : not null String_Access) return Boolean;
+   --  In the given Source file name, replace all occurrences of mold
+   --  variables with the value defined in the Variables map. Behaves
+   --  according to the Settings given and updates the Results object.
+   --
+   --  Parameter source is the name of file with extension "mold", e.g.
+   --  "README.md.mold". The name of the generated file is the same without
+   --  the "mold" extension, "README.md", even when there are no variables to
+   --  replace.
+   --
+   --  Return True if the process end successfully (no error detected).
 
 end Mold_Lib.Impl.File;
--- src/impl/mold_lib-impl-variables.ads
+++ src/impl/mold_lib-impl-variables.ads
@@ -8,19 +8,19 @@
 
 package Mold_Lib.Impl.Variables is
 
-  function Read
-   (Toml_Path : String; Success : out Boolean) return Variables_Map;
-  --  Read all variable definitions of the given TOML Toml_Path. Return a
-  --  Variables_Map object.
+   function Read
+     (Toml_Path : String; Success : out Boolean) return Variables_Map;
+   --  Read all variable definitions of the given TOML Toml_Path. Return a
+   --  Variables_Map object.
 
-  function Apply_Variable_Substitution
-   (Variables : in out Variables_Map) return Boolean;
-  --  Applies the variable substitution process to all variables and update
-  --  their value, if it has changed. Return True no error is found during the
-  --  the variable substitution process.
+   function Apply_Variable_Substitution
+     (Variables : in out Variables_Map) return Boolean;
+   --  Applies the variable substitution process to all variables and update
+   --  their value, if it has changed. Return True no error is found during the
+   --  the variable substitution process.
 
-  function Get_Value (Variable : String) return String;
-  --  Return the value of Variable, if defined. When undefined, it reports an
-  --  error and behaves according to the settings.
+   function Get_Value (Variable : String) return String;
+   --  Return the value of Variable, if defined. When undefined, it reports an
+   --  error and behaves according to the settings.
 
 end Mold_Lib.Impl.Variables;
